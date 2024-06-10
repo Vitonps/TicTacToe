d6f6f1fcdcb3253a3599f3c4d6c73d3c9ee9de3f
VictorCrispim <161848874+Vitonps@users.noreply.github.com>
2024-06-10 17:42:29 -0300

    Update Servidor.java para colocar porta

--- Servidor.java
+++ Servidor.java
@@ -12,20 +12,30 @@ public class Servidor {
     private final Queue<Jogador> filaDeEspera;
 
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Uso: java Servidor <porta>");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
         try {
-            Servidor servidor = new Servidor(12345);
+            Servidor servidor = new Servidor(port);
             servidor.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    //Construtor do servidor
+
+    // Construtor do servidor
     public Servidor(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         filaDeEspera = new LinkedList<>();
     }
-    //Metodo para iniciar o socket do jogador e o StreamHelper
+
+    // Metodo para iniciar o socket do jogador e o StreamHelper
     public void start() {
+        System.out.println("Servidor iniciado e aguardando conexões...");
         while (true) {
             try {
                 Socket jogadorSocket = serverSocket.accept();
@@ -41,7 +51,8 @@ public class Servidor {
             }
         }
     }
-    //Metodo para cuidar da comunicacao entre cliente e servidor
+
+    // Metodo para cuidar da comunicacao entre cliente e servidor
     private void handlePlayerCommunication(Socket jogadorSocket, ObjectInputStream entrada, ObjectOutputStream saida) {
         try {
             String nomeJogador = (String) entrada.readObject();
@@ -51,7 +62,7 @@ public class Servidor {
             System.out.println("Mensagem de escolha de modalidade enviada para: " + nomeJogador);
             String modo = (String) entrada.readObject();
             System.out.println("Modo de jogo escolhido por " + nomeJogador + ": " + modo);
-            //Escolha do modo de jogo
+            // Escolha do modo de jogo
             if (modo.equals("1")) {
                 filaDeEspera.add(new Jogador(jogadorSocket, nomeJogador, entrada, saida));
                 if (filaDeEspera.size() >= 2) {
@@ -62,19 +73,20 @@ public class Servidor {
             } else if (modo.equals("2")) {
                 iniciarJogoContraIA(new JogadorIA(jogadorSocket, nomeJogador, entrada, saida));
             } else {
-                //Se o modo de escolha for errado, desconecta o jogador do servidor
+                // Se o modo de escolha for errado, desconecta o jogador do servidor
                 saida.writeObject("Modo de jogo inválido. Desconectando...");
                 saida.flush();
                 fecharConexao(jogadorSocket, entrada, saida);
                 return;
             }
-        //Retorna erro se algo der errado    
+            // Retorna erro se algo der errado
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             fecharConexao(jogadorSocket, entrada, saida);
         }
     }
-    //Metodo para fechar comunicacao com socket do jogador
+
+    // Metodo para fechar comunicacao com socket do jogador
     private void fecharConexao(Socket jogadorSocket, ObjectInputStream entrada, ObjectOutputStream saida) {
         try {
             if (entrada != null) {
@@ -90,22 +102,25 @@ public class Servidor {
             e.printStackTrace();
         }
     }
-    
-    //Metodo de criacao da thread do jogo multiplayer
+
+    // Metodo de criacao da thread do jogo multiplayer
     private void iniciarJogoMultiplayer(Jogador jogador1, Jogador jogador2) {
         JogoMultiplayer jogoMultiplayer = new JogoMultiplayer(jogador1, jogador2);
         new Thread(jogoMultiplayer).start();
     }
-    //Classe do jogo multiplayer
+
+    // Classe do jogo multiplayer
     private static class JogoMultiplayer implements Runnable {
         private final Jogador jogador1;
         private final Jogador jogador2;
-        //Construtor da classe multiplayer
+
+        // Construtor da classe multiplayer
         public JogoMultiplayer(Jogador jogador1, Jogador jogador2) {
             this.jogador1 = jogador1;
             this.jogador2 = jogador2;
         }
-        //Metodo do jogo multiplayer
+
+        // Metodo do jogo multiplayer
         @Override
         public void run() {
             try {
@@ -114,7 +129,7 @@ public class Servidor {
                 ObjectInputStream entrada1 = jogador1.entrada;
                 ObjectInputStream entrada2 = jogador2.entrada;
                 while (true) {
-                    //Escolha de jogada
+                    // Escolha de jogada
                     String resultado;
                     String msgInicio = "Escolha sua jogada (pedra, papel, tesoura) ou exit para sair: ";
                     saida1.writeObject(msgInicio);
@@ -122,7 +137,7 @@ public class Servidor {
 
                     String jogada1 = (String) entrada1.readObject();
                     String jogada2 = (String) entrada2.readObject();
-                    //Se um dos jogadores escolher "exit" o outro recebe uma mensagem indicando que o jogador saiu
+                    // Se um dos jogadores escolher "exit" o outro recebe uma mensagem indicando que o jogador saiu
                     if (jogada1.equalsIgnoreCase("exit") || jogada2.equalsIgnoreCase("exit")) {
                         String msg = "O outro jogador saiu do jogo ):, fechando o sistema";
                         if (jogada1.equalsIgnoreCase("exit") && !jogada2.equalsIgnoreCase("exit")) {
@@ -134,7 +149,7 @@ public class Servidor {
                         }
                         break;
                     }
-                    //Logica para mostrar o resultado da jogada
+                    // Logica para mostrar o resultado da jogada
                     resultado = determinarResultado(jogada1.toLowerCase(), jogada2.toLowerCase());
                     saida1.writeObject(resultado);
                     saida1.flush();
@@ -148,7 +163,8 @@ public class Servidor {
                 jogador2.fecharConexao();
             }
         }
-        //Metodo para determinar resultado da partida multiplayer
+
+        // Metodo para determinar resultado da partida multiplayer
         private String determinarResultado(String jogada1, String jogada2) {
             String resumo;
 
@@ -159,9 +175,9 @@ public class Servidor {
             } else if ((jogada1.equals("pedra") && jogada2.equals("tesoura")) ||
                     (jogada1.equals("papel") && jogada2.equals("pedra")) ||
                     (jogada1.equals("tesoura") && jogada2.equals("papel"))) {
-                        jogador1.setVitoria();
-                        jogador2.setDerrota();
-                    resumo = jogador1.getNome() + " venceu!";
+                jogador1.setVitoria();
+                jogador2.setDerrota();
+                resumo = jogador1.getNome() + " venceu!";
             } else {
                 jogador2.setVitoria();
                 jogador1.setDerrota();
@@ -169,15 +185,17 @@ public class Servidor {
             }
 
             return jogador1.getNome() + " escolheu " + jogada1 + "\n" + jogador2.getNome() + " escolheu " + jogada2 + "\n" + resumo +
-                   "\nPlacar:\n" + jogador1.getNome() + ": " + jogador1.getResultado() + ",\n" + jogador2.getNome() + ": " + jogador2.getResultado();
+                    "\nPlacar:\n" + jogador1.getNome() + ": " + jogador1.getResultado() + ",\n" + jogador2.getNome() + ": " + jogador2.getResultado();
         }
 
     }
-    //Metodo para iniciar thread do jogo vs IA
+
+    // Metodo para iniciar thread do jogo vs IA
     private void iniciarJogoContraIA(JogadorIA jogadorIA) {
         new Thread(jogadorIA).start();
     }
-    //Classe do jogador
+
+    // Classe do jogador
     private static class Jogador {
         private final Socket socket;
         private final String nome;
@@ -186,7 +204,8 @@ public class Servidor {
         private int vitorias;
         private int derrotas;
         private int empates;
-        //Construtor do jogador
+
+        // Construtor do jogador
         public Jogador(Socket socket, String nome, ObjectInputStream entrada, ObjectOutputStream saida) {
             this.socket = socket;
             this.nome = nome;
@@ -196,35 +215,43 @@ public class Servidor {
             this.derrotas = 0;
             this.empates = 0;
         }
-        //Retorna o nome do jogador
+
+        // Retorna o nome do jogador
         public String getNome() {
             return nome;
         }
-        //Retorna a entrada do jogador
+
+        // Retorna a entrada do jogador
         public ObjectInputStream getEntrada() {
             return entrada;
         }
-        //Retorna a saida do jogador
+
+        // Retorna a saida do jogador
         public ObjectOutputStream getSaida() {
             return saida;
         }
-        //Contador para vitoria
+
+        // Contador para vitoria
         public void setVitoria() {
             vitorias++;
         }
-        //Contador para derrota
+
+        // Contador para derrota
         public void setDerrota() {
             derrotas++;
         }
-        //Contador para empate
+
+        // Contador para empate
         public void setEmpate() {
             empates++;
         }
-        //Metodo para retornar o placar do jogador
+
+        // Metodo para retornar o placar do jogador
         public String getResultado() {
-            return "Vitórias: " + vitorias + " Derrotas: " + derrotas + " Empates: " + empates;
+            return "Vitórias: " + vitorias + ", Derrotas: " + derrotas + ", Empates: " + empates;
         }
-        //Metodo para fechar conexao do jogador
+
+        // Metodo para fechar a conexao do jogador
         public void fecharConexao() {
             try {
                 entrada.close();
@@ -235,57 +262,33 @@ public class Servidor {
             }
         }
     }
-    //Classe da IA do modo jogo IA
-    private static class JogadorIA extends Jogador implements Runnable {
-        private int vitoriasIA;
-        private int derrotasIA;
-        private int empatesIA;
-        //Contador vitoria IA
-        public void setVitoriasIA() {
-            this.vitoriasIA++;
-        }
-        //Contador derrota IA
-        public void setDerrotasIA() {
-            this.derrotasIA++;
-        }
-        //Contador empate IA
-        public void setEmpatesIA() {
-            this.empatesIA++;
-        }
-        //Metodo para retornar o placar da IA
-        public String getResultadoIA() {
-            return "IA - Vitórias: " + vitoriasIA + " Derrotas: " + derrotasIA + " Empates: " + empatesIA;
-        }
-        //Contrutor da IA
-        public JogadorIA(Socket socket, String nome, ObjectInputStream entrada, ObjectOutputStream saida) throws IOException {
-            super(socket, nome, entrada, saida);
 
-            this.vitoriasIA = 0;
-            this.derrotasIA = 0;
-            this.empatesIA = 0;
+    // Classe do jogo contra IA
+    private static class JogadorIA extends Jogador implements Runnable {
+        public JogadorIA(Socket socket, String nome, ObjectInputStream entrada, ObjectOutputStream saida) {
+            super(socket, nome, entrada, saida);
         }
-        //Metodo do jogo vs IA
+
+        // Metodo para rodar o jogo contra IA
         @Override
         public void run() {
             try {
                 ObjectOutputStream saida = getSaida();
                 ObjectInputStream entrada = getEntrada();
                 while (true) {
-                    // Enviar mensagem para escolha da jogada
+                    String resultado;
                     saida.writeObject("Escolha sua jogada (pedra, papel, tesoura) ou exit para sair: ");
                     saida.flush();
-        
                     String jogadaJogador = (String) entrada.readObject();
+                    // Verifica se a escolha do jogador foi "exit" e desconecta do servidor
                     if (jogadaJogador.equalsIgnoreCase("exit")) {
-                        saida.writeObject("Você saiu do jogo.");
+                        saida.writeObject("Você saiu do jogo, desconectando...");
                         saida.flush();
                         break;
                     }
-        
-                    String jogadaIA = determinarJogadaIA();
-                    String resultado = determinarResultado(jogadaJogador, jogadaIA);
-        
-                    // Enviar resultado para o jogador
+                    // Logica para mostrar a escolha aleatoria da IA e enviar ao cliente
+                    String jogadaIA = escolherJogadaIA();
+                    resultado = determinarResultado(jogadaJogador.toLowerCase(), jogadaIA);
                     saida.writeObject(resultado);
                     saida.flush();
                 }
@@ -295,36 +298,36 @@ public class Servidor {
                 fecharConexao();
             }
         }
-        //Metodo para determinar a escolha da IA
-        private String determinarJogadaIA() {
+
+        // Logica da jogada aleatoria da IA
+        private String escolherJogadaIA() {
             String[] jogadas = {"pedra", "papel", "tesoura"};
-            int indice = (int) (Math.random() * jogadas.length);
-            return jogadas[indice];
+            int escolha = (int) (Math.random() * jogadas.length);
+            return jogadas[escolha];
         }
-        //Metodo da logica do resultado do modo IA
+
+        // Logica da escolha da jogada
         private String determinarResultado(String jogadaJogador, String jogadaIA) {
             String resumo;
-    
+
             if (jogadaJogador.equals(jogadaIA)) {
                 resumo = "Empate!";
-                setEmpatesIA();
                 setEmpate();
             } else if ((jogadaJogador.equals("pedra") && jogadaIA.equals("tesoura")) ||
                     (jogadaJogador.equals("papel") && jogadaIA.equals("pedra")) ||
                     (jogadaJogador.equals("tesoura") && jogadaIA.equals("papel"))) {
-                resumo = getNome() + " venceu!";
-                setDerrotasIA();
                 setVitoria();
+                resumo = getNome() + " venceu!";
             } else {
-                resumo = "IA venceu!";
-                setVitoriasIA();
                 setDerrota();
+                resumo = "IA venceu!";
             }
-    
-            return String.format(getNome() + " jogou: " + jogadaJogador + ", \n" + "IA jogou: " + jogadaIA +  ". \n"+
-             resumo +"\n"+ getResultado() + "\n" + getResultadoIA());
+
+            return getNome() + " escolheu " + jogadaJogador + "\n" + "IA escolheu " + jogadaIA + "\n" + resumo +
+                    "\nPlacar:\n" + getNome() + ": " + getResultado();
         }
     }
+
     //Classe StreamHelper
     private static class StreamHelper {
         private final ObjectInputStream entrada;
